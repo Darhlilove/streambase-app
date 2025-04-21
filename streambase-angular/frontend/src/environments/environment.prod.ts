ee22c425c6f625d956aeac1d7ddba30add614f5a
Darhlilove Botchway <botchway.da@northeastern.edu>
2025-04-21 12:32:39 -0400

    Updated environment variables

--- streambase-angular/frontend/src/environments/environment.prod.ts
+++ streambase-angular/frontend/src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
-
-declare const apiKey: string;
-declare const apiUrl: string;
-
-
 export const environment = {
   production: true,
-  TMDB_API_KEY: apiKey,
-  API_URL: apiUrl,
+  TMDB_API_KEY:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMDhhYTQ3OWZjMTAxYWEwNjRjZDNjOTk0ZTE3ODFmZSIsIm5iZiI6MTc0MTAzMTY0Ni41NTUsInN1YiI6IjY3YzYwOGRlZWNlMDFjZWRhMWU3ODVjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fGk9VxyRuwk1j3ZGADMba3v2IHBgQ-v62skpMHD_ivQ',
+  API_URL: 'https://streambase-angular-backend.onrender.com',
 };
--- streambase-angular/frontend/src/environments/environment.ts
+++ streambase-angular/frontend/src/environments/environment.ts
@@ -1,8 +1,6 @@
-declare const apiKey: string;
-
 export const environment = {
   production: false,
-  TMDB_API_KEY: apiKey,
+  TMDB_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMDhhYTQ3OWZjMTAxYWEwNjRjZDNjOTk0ZTE3ODFmZSIsIm5iZiI6MTc0MTAzMTY0Ni41NTUsInN1YiI6IjY3YzYwOGRlZWNlMDFjZWRhMWU3ODVjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fGk9VxyRuwk1j3ZGADMba3v2IHBgQ-v62skpMHD_ivQ",
   API_URL: "http://localhost:3001"
 }
 
